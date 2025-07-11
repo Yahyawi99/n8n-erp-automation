54116c4f1a5ec2282d7c4f8b8f217534900f8a4e
Yahyawi99 <yassinyahyawi26@gmail.com>
2025-07-11 17:18:20 +0100

    working on routes

--- client/src/components/layout/Main.tsx
+++ client/src/components/layout/Main.tsx
@@ -3,18 +3,34 @@ import Header from "./Header";
 // routing
 import {
   createBrowserRouter,
-  Link,
   Route,
   RouterProvider,
   Routes,
 } from "react-router-dom";
+// pages
+import Dashboard from "../../pages/Dashboard";
+import Orders from "../../pages/Orders";
+import Products from "../../pages/Products";
+import Customers from "../../pages/Customers";
+import Invoices from "../../pages/Invoices";
+import Analytics from "../../pages/Analytics";
 
-const router = createBrowserRouter([{ path: "*", element: <Root /> }]);
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <Root />,
+  },
+]);
 
 function Root() {
   return (
     <Routes>
-      <Route path="/header" element={<Header />} />
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/products/*" element={<Products />} />
+      <Route path="/orders/*" element={<Orders />} />
+      <Route path="/customers/*" element={<Customers />} />
+      <Route path="/invoices/*" element={<Invoices />} />
+      <Route path="/Analytics" element={<Analytics />} />
     </Routes>
   );
 }
