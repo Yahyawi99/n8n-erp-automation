af7a778965b70e1bd34ce71b5fe15c8d8acaa1af
Yahyawi99 <yassinyahyawi26@gmail.com>
2025-07-11 11:51:02 +0100

    implemented the base routing

--- client/src/components/layout/Main.tsx
+++ client/src/components/layout/Main.tsx
@@ -1,8 +1,26 @@
 import React from "react";
-// Layouts
+import Header from "./Header";
+// routing
+import {
+  createBrowserRouter,
+  Link,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
+
+const router = createBrowserRouter([{ path: "*", element: <Root /> }]);
+
+function Root() {
+  return (
+    <Routes>
+      <Route path="/header" element={<Header />} />
+    </Routes>
+  );
+}
 
 const Main: React.FC = () => {
-  return <div>Main</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default Main;
